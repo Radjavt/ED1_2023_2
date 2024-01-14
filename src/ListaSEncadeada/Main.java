52865755ba4baf6315d9f601c577612d30873e85
Radija <radjaandrade372@gmail.com>
2024-01-14 18:15:10 -0300

    atualização

--- src/ListaSEncadeada/Main.java
+++ src/ListaSEncadeada/Main.java
@@ -1,10 +1,7 @@
 package ListaSEncadeada;
 
-import java.util.Comparator;
-
 public class Main {
     public static void main(String[] args) {
-        ListaSimpEnc<Integer> listaCorrente = new ListaSimpEnc<>();
         ListaSimpEnc<Integer> listaOriginal = new ListaSimpEnc<>();
         listaOriginal.InserirFim(1);
         listaOriginal.InserirFim(2);
@@ -13,25 +10,10 @@ public class Main {
 
         System.out.println("Lista Original: " + listaOriginal);
 
-        ListaSimpEnc<Integer> novaLista = new ListaSimpEnc<>();
-        novaLista.InserirPartes(listaOriginal, 2);
+        ListaSimpEnc<Integer> ListaCorrente = new ListaSimpEnc<>();
+        ListaCorrente.InserirPartes(listaOriginal, 2);
 
-        System.out.println("Nova Lista após InserirPartes: " + novaLista);
+        System.out.println("Lista Corrente: " + ListaCorrente);
 
-        /*ListaSimpEnc<Integer> lista = new ListaSimpEnc<>();
-        lista.InserirFim(5);
-        lista.InserirFim(7);
-        lista.InserirFim(3);
-        lista.InserirFim(4);
-
-        System.out.println("Lista antes do Insertion Sort:");
-        lista.imprimirLista();
-
-        lista.InsertionSort(Comparator.naturalOrder());
-
-        System.out.println("\nLista depois do Insertion Sort:");
-        lista.imprimirLista();
-
-   */
     }
 }
